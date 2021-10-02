2504e36be7e2621a10a4c413f702d9370fcfdeeb
tacrooms <87332211+tacrooms@users.noreply.github.com>
2021-10-01 23:33:11 -0400

    finished

--- Homework1.java
+++ Homework1.java
@@ -25,10 +25,10 @@ class gradeBook
 		}
 }
 
-class weekday
-{
-	public static void main(String args[])
+	class weekday
 	{
+		public static void main(String args[])
+		{
 		int day;
 		Scanner console = new Scanner(System.in);
 		System.out.print("Enter number 1-7 : ");
@@ -75,5 +75,27 @@ class weekday
 		}
 	}
 }
+
+class weekday
+{
+	public static void main(String args[])
+	{
+		int num = 6;
+
+        if(num % 2 == 1) {
+            System.out.println("Cool!");
+        }
+        else if (num % 2 ==0 && num >=2 && num < 5) {
+            System.out.println("Not Cool!");
+        }
+        else if (num % 2 ==0 && num >=6 && num < 20) {
+            System.out.println("Cool!");
+        }
+
+        else {
+            System.out.println("Not Cool!");
+        }
+	}
+	}
 
 
